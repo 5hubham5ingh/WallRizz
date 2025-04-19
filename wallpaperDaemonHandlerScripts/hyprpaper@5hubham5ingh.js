5d79c6ddf2adf085c6e9f4c8e4db8dafb9891ef3
Nathan Woodburn <github@nathan.woodburn.au>
2025-04-19 17:08:47 +1000

    Update: hyprpaper Handler Update hyprpaper handler to change wallpaper for all screens

--- wallpaperDaemonHandlerScripts/hyprpaper@5hubham5ingh.js
+++ wallpaperDaemonHandlerScripts/hyprpaper@5hubham5ingh.js
@@ -7,5 +7,5 @@
 export function setWallpaper(wallpaperPath) {
   OS.exec(["hyprctl", "-q", "hyprpaper unload all"]);
   OS.exec(["hyprctl", "-q", `hyprpaper preload ${wallpaperPath}`]);
-  OS.exec(["hyprctl", "-q", `hyprpaper wallpaper eDP-1,${wallpaperPath}`]);
+  OS.exec(["hyprctl", "-q", `hyprpaper wallpaper ,${wallpaperPath}`]);
 }
