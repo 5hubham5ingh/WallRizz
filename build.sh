009436695f8fa7ca79a97261b4dfb19df2137aa4
NuOrpheus <160181551+NuOrpheus@users.noreply.github.com>
2025-04-29 20:49:58 +0100

    Update build.sh
    added if statements to prevent issue

--- build.sh
+++ build.sh
@@ -3,24 +3,32 @@
 # Build script for WallRizz
 
 ## Fetch the QuickJS source code, then build and install the compiler and interpreter in the system.
-echo -e "\e[1;4;33mFetching source code...\e[0m" &&
-  git clone --depth 1 https://github.com/bellard/quickjs.git &&
-  cd quickjs &&
-  make &&
-  sudo make install &&
-  cd .. &&
+  if ! [ -d "quickjs" ]; then
+     echo -e "\e[1;4;33mFetching source code...\e[0m" &&
+     git clone --depth 1 https://github.com/bellard/quickjs.git &&
+     cd quickjs &&
+     make &&
+     sudo make install &&
+     cd .. 
+  fi &&
 
   ## Fetch the required library.
-  curl -L -o out.zip https://github.com/ctn-malone/qjs-ext-lib/archive/refs/tags/0.12.4.zip &&
-  unzip out.zip &&
-  mv qjs-ext-lib-0.12.4 qjs-ext-lib &&
-  rm out.zip &&
+  if ! [ -d "qjs-ext-lib-0.12.4" ]; then
+     curl -L -o out.zip https://github.com/ctn-malone/qjs-ext-lib/archive/refs/tags/0.12.4.zip &&
+     unzip out.zip &&
+     mv qjs-ext-lib-0.12.4 qjs-ext-lib &&
+     rm out.zip
+  fi &&
 
   ## Fetch helper scripts
-  git clone --depth 1 https://github.com/5hubham5ingh/justjs.git &&
+  if ! [ -d "justjs" ]; then
+     git clone --depth 1 https://github.com/5hubham5ingh/justjs.git  
+  fi &&
 
   ## Clone the WallRizz project
-  git clone --depth 1 https://github.com/5hubham5ingh/WallRizz.git &&
+  if ! [ -d "WallRizz" ]; then
+     git clone --depth 1 https://github.com/5hubham5ingh/WallRizz.git
+  fi &&
 
   ## Build WallRizz then install it.
   cd WallRizz/src &&
